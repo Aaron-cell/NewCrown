4a537e23de915e61863ed7e17f4e8e3e24ee6b58
Aaron <1464889572@qq.com>
2020-09-07 17:55:59 +0800

    解决了在windows和linux环境中都能获取项目根路径 以及 更改了生产环境下mysql配置

--- utils/RootPath.py
+++ utils/RootPath.py
@@ -1,9 +1,24 @@
 #获取项目根路径
 import os
+import platform
 
 def getRoot_Path():
+    """
+    对当前运行的系统判断，得到相应正确的项目根路径
+    :return:
+    """
+    system = platform.system()
     curPath = os.path.abspath(os.path.dirname(__file__))
-    rootPath = curPath[:curPath.find("NewCrown\\")+len("NewCrown\\")]  # 获取myProject，也就是项目的根路径
-    rootPath = str(rootPath).replace('\\','/')#路径\换成/
+    if system == 'Windows':
+        rootPath = curPath[:curPath.find("NewCrown\\")+len("NewCrown\\")]  # 获取myProject，也就是项目的根路径
+        rootPath = str(rootPath).replace('\\','/')#路径\换成/   windows环境下
+    elif system == 'Linux':
+        rootPath = curPath[:curPath.find("NewCrown/")+len("NewCrown/")] #Linux环境下
+    else:
+        print('当前系统类型无法识别')
     return rootPath
+
+if __name__ == '__main__':
+    rootPath = getRoot_Path()
+    print(rootPath)
 
